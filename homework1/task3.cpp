1ae3120447e667f23e912aa7658c20b8b99f7329
arullka <145289785+arullka@users.noreply.github.com>
2026-02-22 19:21:42 +0300

    Update task3.cpp

--- homework1/task3.cpp
+++ homework1/task3.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+int main()
+{
+	long long seconds;
+	std::cin >> seconds;
+
+	const long long sec_in_week = 604800;
+	const long long sec_in_day = 86400;
+	const long long sec_in_hour = 3600;
+	const long long sec_in_min = 60;
+
+	std::cout << seconds / sec_in_week << " weeks, ";
+	seconds %= sec_in_week;
+	std::cout << seconds / sec_in_day << " days, ";
+	seconds %= sec_in_day;
+	std::cout << seconds / sec_in_hour << " hours, ";
+	seconds %= sec_in_hour;
+	std::cout << seconds / sec_in_min << " minutes, ";
+	seconds %= sec_in_min;
+	std::cout << seconds << " seconds";
+}
